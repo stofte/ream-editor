fae349397c1a5394ff96c229e64d3de98f9908c5
stofte <stofte@gmail.com>
2015-02-01 02:41:21 +0100

    travis ci

--- LinqEditor.Core.Templates.Tests/Properties/AssemblyInfo.cs
+++ LinqEditor.Core.Templates.Tests/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("LinqEditor.Core.Templates.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("KMD A/S")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LinqEditor.Core.Templates.Tests")]
-[assembly: AssemblyCopyright("Copyright © KMD A/S 2015")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

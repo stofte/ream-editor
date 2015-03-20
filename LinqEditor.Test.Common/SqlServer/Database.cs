b083104185016fb9763a019f09c50723175331a9
stofte <stofte@gmail.com>
2015-03-20 12:34:32 +0100

    use mssql2012 instead

--- LinqEditor.Test.Common/SqlServer/Database.cs
+++ LinqEditor.Test.Common/SqlServer/Database.cs
@@ -13,7 +13,7 @@ namespace LinqEditor.Test.Common.SqlServer
             {
                 if (Environment.GetEnvironmentVariable("CI") == "True")
                 {
-                    return @"Server=(local)\SQL2014;Initial Catalog=master;Integrated Security=True";
+                    return @"Server=(local)\SQL2012SP1;Initial Catalog=master;Integrated Security=True";
                 }
                 return @"Data Source=(LocalDB)\v11.0;Initial Catalog=master;Integrated Security=True"; 
             } 

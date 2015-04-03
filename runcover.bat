0e1a3e300fa58276e0e73fcd4c160ad748b382d0
stofte <stofte@gmail.com>
2015-04-03 17:34:55 +0200

    reportgeneration is locally only

--- runcover.bat
+++ runcover.bat
@@ -7,8 +7,8 @@ set configuration=Release
 mkdir %folder%
 :run
 .\packages\OpenCover.4.5.3723\OpenCover.Console.exe -target:runtests.bat -register:user -filter:"+[LinqEditor.Core*]* -[*Tests]*" -output:.\%folder%\TestCoverage.xml
-.\packages\ReportGenerator.2.1.4.0\reportgenerator.exe -reports:.\%folder%\TestCoverage.xml -targetdir:%folder%
 if "True" == "%CI%" goto :skipdisplay
+.\packages\ReportGenerator.2.1.4.0\reportgenerator.exe -reports:.\%folder%\TestCoverage.xml -targetdir:%folder%
 start .\%folder%\index.htm
 :skipdisplay
 if "_" == "%CI%_" goto :skipupload

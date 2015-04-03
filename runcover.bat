20f5e4a25039a57c3b011c0ec80e806cd947eb1f
stofte <stofte@gmail.com>
2015-04-03 16:48:01 +0200

    coveralls.io integration

--- runcover.bat
+++ runcover.bat
@@ -1,5 +1,15 @@
 set folder=TestCoverage
+if "True" == "%CI%" goto :ci
 set configuration=Debug
+goto :run
+:ci
+set configuration=Release
+:run
 .\packages\OpenCover.4.5.3723\OpenCover.Console.exe -target:runtests.bat -register:user -filter:"+[LinqEditor.Core*]* -[*Tests]*" -output:.\%folder%\TestCoverage.xml
-.\packages\ReportGenerator.2.1.4.0\reportgenerator.exe -reports:.\%folder%\TestCoverage.xml -targetdir:%folder%  
+.\packages\ReportGenerator.2.1.4.0\reportgenerator.exe -reports:.\%folder%\TestCoverage.xml -targetdir:%folder%
+if "True" == "%CI%" goto :skipdisplay
 start .\%folder%\index.htm
+:skipdisplay
+if "_" == "%CI%_" goto :skipupload
+.\packages\coveralls.io.1.3.4\tools\coveralls.net.exe -opencover %folder%\TestCoverage.xml
+:skipupload

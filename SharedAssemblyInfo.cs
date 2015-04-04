29bbe7a30a50d693332599a8601765aef9f663cb
stofte <stofte@gmail.com>
2015-04-04 21:21:19 +0200

    organized assemblyinfo /w. shared info

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("LinqEditor")]
+// patched by ci
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyCopyright("Copyright © Svend Ezaki Tofte")]
+[assembly: ComVisible(false)]

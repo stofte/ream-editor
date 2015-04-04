d3ffe2079d6ec342dc12587ece2b663b2bdd665b
stofte <stofte@gmail.com>
2015-04-05 00:16:38 +0200

    use appveyors test runner

--- runtests.bat
+++ runtests.bat
@@ -1,4 +1,7 @@
-.\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe ^
+if "True" == "%CI%" goto :run
+set nunitpath=.\packages\NUnit.Runners.2.6.4\tools\
+:run
+%nunitpath%nunit-console.exe ^
 LinqEditor.Core.CodeAnalysis.Tests\bin\%CONFIGURATION%\LinqEditor.Core.CodeAnalysis.Tests.dll ^
 LinqEditor.Core.Session.Tests\bin\%CONFIGURATION%\LinqEditor.Core.Session.Tests.dll ^
 LinqEditor.Core.Templates.Tests\bin\%CONFIGURATION%\LinqEditor.Core.Templates.Tests.dll ^

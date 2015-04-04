5a1546562b45f19036c3b85e6a9b334502d5e3b9
stofte <stofte@gmail.com>
2015-04-04 20:04:01 +0200

    another build test

--- runtests.bat
+++ runtests.bat
@@ -1,1 +1,6 @@
-.\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe LinqEditor.Core.CodeAnalysis.Tests\bin\%CONFIGURATION%\LinqEditor.Core.CodeAnalysis.Tests.dll LinqEditor.Core.Session.Tests\bin\%CONFIGURATION%\LinqEditor.Core.Session.Tests.dll LinqEditor.Core.Templates.Tests\bin\%CONFIGURATION%\LinqEditor.Core.Templates.Tests.dll LinqEditor.Core.Tests\bin\%CONFIGURATION%\LinqEditor.Core.Tests.dll /noshadow
+.\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe ^
+LinqEditor.Core.CodeAnalysis.Tests\bin\%CONFIGURATION%\LinqEditor.Core.CodeAnalysis.Tests.dll ^
+LinqEditor.Core.Session.Tests\bin\%CONFIGURATION%\LinqEditor.Core.Session.Tests.dll ^
+LinqEditor.Core.Templates.Tests\bin\%CONFIGURATION%\LinqEditor.Core.Templates.Tests.dll ^
+LinqEditor.Core.Tests\bin\%CONFIGURATION%\LinqEditor.Core.Tests.dll ^
+/noshadow

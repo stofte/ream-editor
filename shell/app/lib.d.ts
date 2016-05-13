0f5202dd9b89696400251a1274655baf604c83af
Svend Tofte <stofte@gmail.com>
2016-05-13 02:12:40 +0200

    fix type errors

--- shell/app/lib.d.ts
+++ shell/app/lib.d.ts
@@ -1,3 +1,11 @@
+// tsdm doesn't include this file
+/// <reference path="../node_modules/retyped-codemirror-tsd-ambient/codemirror-showhint.d.ts" />
+
+declare module CodeMirror {
+    var keyMap: any;
+    var hint: any;
+}
+
 declare var electronRequire: any;
 declare var __dirname: string;
 

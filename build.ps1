88a8368f2a9a36e17b00aa8e8de5e58029dc979b
Svend Tofte <stofte@gmail.com>
2016-05-14 20:37:24 +0200

    building locally as well

--- build.ps1
+++ build.ps1
@@ -1,8 +1,17 @@
-copy index.html package.json systemjs.config.js electron-main.js $env:PACKAGE_BASE
+# if this is a local build, this is the assumed layout
+if (-Not($env:PACKAGE_BASE)) { $env:PACKAGE_BASE="build" }
+if (-Not($env:ELECTRON_OUT)) { $env:ELECTRON_OUT="linq-editor-win32-x64" }
+if (-Not($env:DOTNET_INSTALL_DIR)) { $env:DOTNET_INSTALL_DIR="C:\Program Files\dotnet" }
+if (-Not($env:OMNISHARP_ZIP)) { $env:OMNISHARP_ZIP="omnisharp-win-x64-netcoreapp1.0.zip" }
+mkdir -Force $env:PACKAGE_BASE | Out-Null
+copy index.html $env:PACKAGE_BASE
+copy package.json $env:PACKAGE_BASE 
+copy systemjs.config.js $env:PACKAGE_BASE
+copy electron-main.js $env:PACKAGE_BASE 
 npm run ts-build
 npm run bundle $env:PACKAGE_BASE
-dotnet restore
-dotnet publish --configuration Release --output "$env:PACKAGE_BASE\query"
+# dotnet restore
+dotnet publish --configuration Release --output $env:PACKAGE_BASE\query
 # windows dotnet cant make exe files, so need to include dotnet.exe for bootstraping query-engine
 copy "$env:DOTNET_INSTALL_DIR\dotnet.exe" $env:PACKAGE_BASE\query\dotnet.exe
 copy "$env:DOTNET_INSTALL_DIR\hostfxr.dll" $env:PACKAGE_BASE\query\hostfxr.dll
@@ -12,7 +21,7 @@ copy NuGet.Config $env:PACKAGE_BASE\query\NuGet.Config
 copy project.json $env:PACKAGE_BASE\query\project.json
 copy project.lock.json $env:PACKAGE_BASE\query\project.lock.json
 # include omnisharp in dist
-7z x $env:OMNISHARP_ZIP -y -o$env:PACKAGE_BASE\omnisharp
+7z x $env:OMNISHARP_ZIP -y -o"$env:PACKAGE_BASE\omnisharp"
 # bundle everything in the package folder
-npm run electron_package_appveyor
+npm run package_electron
 7z a $env:ELECTRON_OUT.zip $env:ELECTRON_OUT

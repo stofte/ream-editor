89e9eed1ab2fb6137f818c10a340f67d8522cd9d
Svend Tofte <stofte@gmail.com>
2016-05-24 22:54:16 +0200

    Added batch file replacement for build.ps1

---
+++ build_script.cmd
@@ -0,0 +1,30 @@
+@echo off
+if "%CI%" == "" (
+    set PACKAGE_BASE="build"
+    set ELECTRON_OUT="linq-editor-win32-x64"
+    set OMNISHARP_ZIP="omnisharp-win-x64-netcoreapp1.zip"
+    rem https://github.com/npm/npm/issues/2938#issuecomment-11337463
+    set NPM="C:\Program Files (x86)\nodejs\npm.cmd"
+    rmdir /q /s %PACKAGE_BASE%
+)
+mkdir %PACKAGE_BASE%
+call %NPM% install
+call %NPM% run lint
+call %NPM% run ts-build
+call %NPM% run bundle %PACKAGE_BASE%
+copy index.static.html %PACKAGE_BASE%\index.html
+copy electron-main.js %PACKAGE_BASE%\electron-main.js
+copy omnisharp-setup.js %PACKAGE_BASE%\omnisharp-setup.js
+copy package.json %PACKAGE_BASE%\package.json
+mkdir %PACKAGE_BASE%\node_modules\bootstrap\dist\fonts
+copy node_modules\bootstrap\dist\fonts\*.* %PACKAGE_BASE%\node_modules\bootstrap\dist\fonts\
+rem dotnet restore
+dotnet publish --configuration Release --output %PACKAGE_BASE%\query --runtime win7-x64 --framework netcoreapp1.0
+copy NuGet.Config %PACKAGE_BASE%\NuGet.Config
+copy project.json %PACKAGE_BASE%\project.json
+copy project.lock.json %PACKAGE_BASE%\project.lock.json
+copy NuGet.Config %PACKAGE_BASE%\query\NuGet.Config
+copy project.json %PACKAGE_BASE%\query\project.json
+copy project.lock.json %PACKAGE_BASE%\query\project.lock.json
+7z x %OMNISHARP_ZIP% -y -o"%PACKAGE_BASE%\omnisharp"
+call %NPM% run package_electron

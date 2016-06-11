7424bc6cb8673cb9b67762140bffc5787416e2be
Svend Tofte <stofte@gmail.com>
2016-06-11 17:41:21 +0200

    Model index

---
+++ app/models/index.ts
@@ -0,0 +1,27 @@
+import { AutocompletionQuery } from './autocompletion-query';
+import { AutocompletionResult } from './autocompletion-result';
+import { CodeCheckResult } from './code-check-result';
+import { CompletionItem } from './completion-item';
+import { Connection } from './connection';
+import { EditorChange } from './editor-change';
+import { QueryRequest } from './query-request';
+import { QueryResult } from './query-result';
+import { ResultPage } from './result-page';
+import { ResultStore } from './result-store';
+import { Tab } from './tab';
+import { TemplateResult } from './template-result';
+
+export default {
+    AutocompletionQuery,
+    AutocompletionResult,
+    CodeCheckResult,
+    CompletionItem,
+    Connection,
+    EditorChange,
+    QueryRequest,
+    QueryResult,
+    ResultPage,
+    ResultStore,
+    Tab,
+    TemplateResult
+};

7e3d0d3a98662020b02ef72a96097cef076fd44a
Svend Tofte <stofte@gmail.com>
2016-06-12 19:26:08 +0200

    sh syntax

--- build_script.sh
+++ build_script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-if [ $TRAVIS? -eq 0 ]; then
+if [ $TRAVIS -neq "true"  ]; then
     export PACKAGE_BASE=build
     export ELECTRON_OUT=linq-editor-ubuntu-x64
     export OMNISHARP_ZIP=omnisharp-ubuntu-x64-netcoreapp1.0.tar.gz

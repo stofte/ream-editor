d5b7d8e33f3c96b6303a4ad6f1ff993d644fcb07
Svend Tofte <stofte@gmail.com>
2016-06-13 14:06:45 +0200

    Fix paths for local linux builds

--- build_script.sh
+++ build_script.sh
@@ -3,7 +3,7 @@ if [ "$TRAVIS" = "" ];
 then
     echo "local build"
     export PACKAGE_BASE=build
-    export ELECTRON_OUT=linq-editor-ubuntu-x64
+    export ELECTRON_OUT=linq-editor-linux-x64
     export OMNISHARP_ZIP=omnisharp-ubuntu-x64-netcoreapp1.0.tar.gz
     rm -rf $PACKAGE_BASE
 else

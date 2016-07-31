825e418a948496eb40d54f5706ee9c13801c6921
Svend Tofte <stofte@gmail.com>
2016-08-01 01:16:04 +0200

    Setup mocha env

---
+++ test/mocha-spec-setup.js
@@ -0,0 +1,11 @@
+// replaces index.html's shenanigans when using mocha.
+global.electronRequire = require;
+global.IS_WINDOWS= true;
+global.IS_LINUX = false;
+global.MODE = 'DEVELOPMENT';
+global.Assert = function Assert(cond, msg) {
+    if (!cond) {
+        throw (msg ? msg : 'assert failed');
+    }
+}
+global.XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;

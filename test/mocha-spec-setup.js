7a1bca1a7ad703ffdef09aa4ccbdbcb780b2e928
Svend Tofte <stofte@gmail.com>
2016-09-19 19:25:17 +0200

    Test platform detection

--- test/mocha-spec-setup.js
+++ test/mocha-spec-setup.js
@@ -1,7 +1,8 @@
 // replaces index.html's shenanigans when using mocha.
 global.electronRequire = require;
-global.IS_WINDOWS= true;
-global.IS_LINUX = false;
+const isWindows = process.platform === 'win32';
+global.IS_WINDOWS = isWindows;
+global.IS_LINUX = !isWindows; // todo
 global.MODE = 'DEVELOPMENT';
 global.Assert = function Assert(cond, msg) {
     if (!cond) {

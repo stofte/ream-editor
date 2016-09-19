2953d1092efb56d4d674fe2d3e17b10fdefe81c4
Svend Tofte <stofte@gmail.com>
2016-09-19 19:26:12 +0200

    Adjust runtime path for test

--- app/config.ts
+++ app/config.ts
@@ -15,6 +15,6 @@ export default {
     omnisharpPort: 2000,
     queryEnginePort: 8111,
     omnisharpProjectPath: omnisharpPath,
-    dotnetDebugPath: IS_LINUX ? path.normalize('/dotnetpreview2/dotnet')
+    dotnetDebugPath: IS_LINUX ? path.normalize('/usr/bin/dotnet')
         : path.normalize('C:/Program Files/dotnet/dotnet.exe')  
 };

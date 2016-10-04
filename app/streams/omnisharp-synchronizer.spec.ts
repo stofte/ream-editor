2a1e38171a172118064086281587f966426a0694
Svend Tofte <stofte@gmail.com>
2016-10-04 02:41:33 +0200

    Rewrote synchronizer and testsuite

--- app/streams/omnisharp-synchronizer.spec.ts
+++ app/streams/omnisharp-synchronizer.spec.ts
@@ -1,7 +1,5 @@
 import * as chai from 'chai';
 import { expect } from 'chai';
-import * as sinon from 'sinon';
-import * as sinonChai from 'sinon-chai';
 import { OmnisharpSessionMessage } from '../messages/index';
 import { OmnisharpSynchronizer } from './omnisharp-synchronizer';
 import { Observable, Subject } from 'rxjs/Rx';
@@ -13,82 +11,137 @@ describe('omnisharp-synchronizer', function() {
     
     before(function() {
         chai.expect();
-        chai.use(sinonChai);
     });
 
     beforeEach(function() {
         sync = new OmnisharpSynchronizer();
     });
-    
-    it('resolves events in expected order', function(done) {
-        this.timeout(20000);
-        const timestamps: any[] = [];
-        const sessionId = uuid.v4();
-        const subject = new Subject<OmnisharpSessionMessage>()
-        let subjectCount = 0;
-        let stepResolver = null; 
-        const syntheticDelayMs = 250;
-        const stepsDone = new Promise((done) => stepResolver = done);
-        const stepsToReplay = [
-            () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'context' }),
-            () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'edit' }),
-            // the template arrives a bit later
-            () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'buffer-template' }),
-            () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'edit' }),
-            // these should be executed without blocking each other
-            () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'codecheck' }),
-/* idx 5 */ () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'autocompletion' }),
-            // these depend on each other sequentially
-/* idx 6 */ () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'edit' }),
-            () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'codecheck' }),
-            () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'edit' }),
-            () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'autocompletion' }),
-            // switch context
-/* idx 10 */() => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'context' }),
-            () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'edit' }),
-            () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'edit' }),
-            () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'codecheck' }),
-            () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'autocompletion' }),
-            () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'buffer-template' }),
-        ];
-        // setup stream
-        let idx = 0;
-        subject 
-            .delayWhen(msg => Observable.fromPromise(sync.queueOperation(msg)))
-            .delay(syntheticDelayMs)
-            .subscribe(msg => {
-                // console.log('\n\n' + (idx++) + ' ' + msg.type.substring(0, 5) + '\t' + msg.timestamp);
-                subjectCount++;
-                sync.resolveOperation(msg);
-                timestamps.push({ timestamp: msg.timestamp, resolvedTimestamp: performance.now(), type: msg.type });
-                if (subjectCount >= stepsToReplay.length) stepResolver();
+    // first number in list is min delay
+    [
+        [  10, 1, 1, 2]
+        // these steps can take upward of an hour due to randomization in test
+        // [  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [100, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [100, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [100, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [100, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [100, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [200, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [200, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [200, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [200, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [200, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [250, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [250, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [250, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [250, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [250, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [500, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [500, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [500, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [500, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20],
+        // [500, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20]
+    ].forEach((runModifiers, idx) => {
+        const minDelayMs = runModifiers[0];
+        runModifiers.slice(1).forEach((modifier: number, idx2) => {
+            const maxDelayMs = 50 * modifier;
+            it(`resolves events in expected order (run ${idx < 10 ? '0': ''}${idx}, step ${idx2 < 10 ? '0': ''}${idx2}, delay ${minDelayMs}ms ~ ${maxDelayMs}ms)`, function(done) {
+                omnisharpSynchronizerSuite.bind(this)(minDelayMs, maxDelayMs, sync, done);
             });
-        // start to inject events
-        replaySteps(stepsToReplay);
-        // check results
-        stepsDone.then(() => {
-            try {
-                const eventOrder = timestamps.map(x => x.type).join(',');
-                const r = '(codecheck|autocompletion)';
-                const firstOrder = `^context,buffer-template,edit,edit,${r},${r},edit,codecheck,edit,autocompletion`;
-                const secondOrder = `${firstOrder},context,buffer-template,edit,edit,${r},${r}$`;
-                const expectedOrder = new RegExp(secondOrder);
-                expect(timestamps.length).to.equal(stepsToReplay.length);
-                // console.log('\n\n' + timestamps.map(x => x.type.substring(0, 7) + '\t' + x.resolvedTimestamp).join('\n'));
-                expect(eventOrder).to.match(expectedOrder);
-                timestamps.forEach((ts, idx) => {
-                    if (idx === 5) {
-                        // these two events should be executed without blocking as the operations depend on the same buffer
-                        expect(ts.resolvedTimestamp - timestamps[idx - 1].resolvedTimestamp).to.be.lessThan(syntheticDelayMs, `Idx ${idx}`);
-                    } else if (idx > 5 && idx < 10) {
-                        // operations depend on the previous, so should be greater then the synthetic lag
-                        expect(ts.resolvedTimestamp - timestamps[idx - 1].resolvedTimestamp).to.be.greaterThan(syntheticDelayMs, `Idx ${idx}`);
-                    }
-                });
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
+        })
     });
 });
+
+function omnisharpSynchronizerSuite(minDelayMs: number, maxDelayMs: number, sync: OmnisharpSynchronizer, done: Function) {
+    this.timeout(((minDelayMs + maxDelayMs) * 2 * 20) + 5000);
+    const timestamps: any[] = [];
+    const sessionId = uuid.v4();
+    const subject = new Subject<OmnisharpSessionMessage>()
+    let subjectCount = 0;
+    let stepResolver = null;
+    const stepsDone = new Promise((done) => stepResolver = done);
+    const stepsToReplay = [
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'context' }),
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'edit' }),
+        // the template arrives a bit later
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'buffer-template' }),
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'edit' }),
+        // these should be executed without blocking each other
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'codecheck' }),
+/* 5 */ () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'autocompletion' }),
+        // these depend on each other sequentially
+/* 6 */ () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'edit' }),
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'codecheck' }),
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'edit' }),
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'autocompletion' }),
+        // switch context
+/* 10 */() => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'context' }),
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'edit' }),
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'edit' }),
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'codecheck' }),
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'autocompletion' }),
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'buffer-template' }),
+        // switch context
+/* 16 */() => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'context' }),
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'codecheck' }),
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'buffer-template' }),
+        () => subject.next(<OmnisharpSessionMessage> { sessionId, timestamp: performance.now(), type: 'edit' })
+    ];
+    // setup stream
+    let idx = 0;
+    subject 
+        .delayWhen(msg => Observable.fromPromise(sync.queueOperation(msg)))
+        .subscribe(msg => {
+            // context msgs should not be delayed, since it doesnt go to omnisharp
+            const timeout = msg.type === 'context' ? 0 : (Math.random() * maxDelayMs) + minDelayMs;
+            timestamps.push({ timestamp: msg.timestamp, resolvedTimestamp: performance.now(), type: msg.type });
+            subjectCount++;
+            setTimeout(() => {
+                sync.resolveOperation(msg);
+                // seems more stable if we add a slight delay here?
+                if (subjectCount >= stepsToReplay.length) setTimeout(() => stepResolver(), 100);
+            }, timeout);
+        });
+    // start to inject events
+    replaySteps(stepsToReplay, maxDelayMs, minDelayMs);
+    // check results
+    stepsDone.then(() => {
+        try {
+            const events = timestamps.map(x => x.type);
+            timestamps.forEach((x, idx) => {
+                if (idx > 1) {
+                    expect(x.resolvedTimestamp).to.be.greaterThan(timestamps[idx - 1].resolvedTimestamp);
+                }
+            })
+            expect(timestamps.length).to.equal(stepsToReplay.length);
+            expect(events.slice(0, 4)).to.deep.equal(['context', 'buffer-template', 'edit', 'edit'], 'Idx 0-3');
+            expect(events.slice(4, 6).sort()).to.deep.equal(['autocompletion','codecheck'], 'Idx 4-5'); // might be interleaved
+            expect(events.slice(6, 10)).to.deep.equal(['edit', 'codecheck', 'edit', 'autocompletion'], 'Idx 6-9');
+            expect(events.slice(10, 14)).to.deep.equal(['context', 'buffer-template', 'edit', 'edit'], 'Idx 10-13');
+            expect(events.slice(14, 16).sort()).to.deep.equal(['autocompletion','codecheck'], 'Idx 14-15'); // might be interleaved
+            expect(events.slice(16, 20)).to.deep.equal(['context','buffer-template', 'codecheck', 'edit'], 'Idx 16-19');
+
+            const dependents = timestamps.filter(x => x.type === 'edit' || x.type === 'codecheck');
+            const initTimestamps = dependents.map(x => x.timestamp);
+            const doneTimestamps = dependents.map(x => x.resolvedTimestamp);
+            // check that dependents were not reordered
+            initTimestamps.forEach((x, idx) => {
+                if (idx > 0) {
+                    expect(x).to.be.greaterThan(initTimestamps[idx - 1], `Init timestamp idx ${idx}`);
+                    expect(doneTimestamps[idx]).to.be.greaterThan(doneTimestamps[idx - 1], `Done timestamp idx ${idx}`);
+                }
+            });
+            done();
+        } catch (e) {
+            // console.log('BOOOOOOOOOOOOOOOOOOOOOOOOOOOM')
+            // console.log(e)
+            // process.exit();
+            done(e);
+        }
+    });
+}
--- app/streams/omnisharp-synchronizer.ts
+++ app/streams/omnisharp-synchronizer.ts
@@ -8,122 +8,111 @@ class Queued {
 
 class SessionState {
     public id: string;
-    public ready: boolean;
-    public operationsInFlight: number;
-    public lockedByOperation: boolean;
-    public lockedByEdit: boolean;
-    public queue: Queued[];
-}
+    public ready: boolean = false;
+    public fileName: string = null;
+    public columnOffset: number = null;
+    public lineOffset: number = null;
+    public queue: Queued[] = [];
+    public awaiting: Queued[] = [];
 
-export class OmnisharpSynchronizer {
-    private session: SessionState[] = [];
-    
-    public resolveOperation(msg: OmnisharpSessionMessage): void {
-        const session = this.getSession(msg);
-        this.releaseSession(msg.type, session);
-        
-        // if (!session.lockedByOperation && session.queue.length > 0) {
-        //     const nextOp = session.queue.shift();
-        //     this.aquireSession(nextOp.msg.type, session);
-        //     nextOp.dequeue();
-        //     if (!session.lockedByEdit) {
-        //         // dequeue all other non-edit operations on the buffer
-        //         while (session.queue.length > 0) {
-        //             const nextType = session.queue[0].msg.type;
-        //             if (nextType === 'edit' || nextType === 'context') {
-        //                 break;
-        //             }
-        //             const nextOpOp = session.queue.shift();
-        //             session.operationsInFlight++;
-        //             nextOpOp.dequeue();
-        //         }
-        //     }
-        // }
+    constructor(id: string) {
+        this.id = id;
     }
 
     public queueOperation(msg: OmnisharpSessionMessage): Promise<number> {
-        const session = this.getSession(msg);
-        const isOperation = msg.type === 'codecheck' || msg.type === 'autocompletion';
-        const isBufferTemplate = msg.type === 'buffer-template';
-        const isContext = msg.type === 'context';
-        const isEdit = msg.type === 'edit';
-        const isBlocked = this.isBlocked(msg);
-        // console.log('queueOperation.isBlocked', msg.type, isBlocked)
-        if (isBlocked) {
+        if (this.isBlocked(msg)) {
+            // console.log(`queueOperation BLOCK ${msg.type}:${msg.timestamp}`);
             let dequeue: Function = null;
             const p = new Promise((done) => dequeue = done);
-            session.queue.push({msg , dequeue});
+            this.queue.push({msg , dequeue});
             return p;
         } else {
-            this.aquireSession(msg.type, session);
+            // console.log(`queueOperation PASS ${msg.type}:${msg.timestamp}`);
+            if (msg.type === 'context') {
+                this.ready = false; // if we pass a context, we immediatly invalidate the session
+            } 
+            this.awaiting.push({msg, dequeue: null});
             return new Promise((done) => done(1));
         }
     }
 
-    private isBlocked(msg: OmnisharpSessionMessage): boolean {
-        const session = this.getSession(msg);
-        if (msg.type === 'codecheck' || msg.type === 'autocompletion') {
-            return session.lockedByEdit || !session.ready;
-        } else if (msg.type === 'buffer-template') {
-            return session.lockedByEdit || session.lockedByOperation;
-        } else if (msg.type === 'context') {
-            return session.lockedByEdit || session.lockedByOperation;
+    public resolveOperation(msg: OmnisharpSessionMessage): void {
+        // todo would be nice with internal id
+        const op = this.awaiting.find(x => x.msg.timestamp === msg.timestamp);
+        Assert(op !== null, 'resolveOperation msg found in awaiting queue');
+        const prevLength = this.awaiting.length;
+        this.awaiting = this.awaiting.filter(x => x.msg.timestamp !== msg.timestamp);
+        Assert(prevLength === this.awaiting.length + 1, 'Removed single element from awaiting');
+        if (msg.type === 'buffer-template') {
+            this.lineOffset = msg.lineOffset;
+            this.columnOffset = msg.columnOffset;
+            this.fileName = msg.fileName;
+            this.ready = true;
+        }
+        // console.log('resolveOperation.awaiting', this.awaiting.map(x => x.msg.type + ':' + x.msg.timestamp));
+        // console.log('resolveOperation.queue', this.queue.map(x => x.msg.type + ':' + x.msg.timestamp));
+        // determine if any queued operations should be resolved
+        if (this.awaiting.length === 0) {
+            if (this.ready) {
+                if (this.queue.length > 0) {
+                    const multOpsIdx = this.getInitalOperationCount();
+                    if (multOpsIdx) {
+                        Assert(this.queue[0].msg.type === 'codecheck' || this.queue[0].msg.type === 'autocompletion', 'First element is expected operation');
+                        // we have multiple codecheck/autocompletion ops queued on the same buffer timestamp
+                        const ops = this.queue.splice(0, multOpsIdx);
+                        Assert(ops.length > 0 && ops.map(x => x.msg.type === 'codecheck' || x.msg.type === 'autocompletion'), 'Found multiple ops');
+                        ops.forEach(x => {
+                            this.awaiting.push(x);
+                            // console.log('multi dequeue', x.msg.type + ':' + x.msg.timestamp);
+                            setTimeout(() => x.dequeue());
+                        });
+                    } else {
+                        // otherwise, we only dequeue the next operation 
+                        const nextOp = this.queue.shift();
+                        this.awaiting.push(nextOp);
+                        // console.log('single dequeue', nextOp.msg.type + ':' + nextOp.msg.timestamp);
+                        if (nextOp.msg.type === 'context') {
+                            // if we dequeue a context, we instantly flag the session as invalid
+                            this.ready = false;
+                        }
+                        setTimeout(() => nextOp.dequeue());
+                    }
+                }
+            } else {
+                // scan for a queued buffer-template and prioritize it to resolve the lock
+                const index = this.getBufferTemplateOffset();
+                if (index) {
+                    const nextOp = this.queue.splice(index, 1)[0];
+                    this.awaiting.push(nextOp);
+                    setTimeout(() => nextOp.dequeue());
+                }
+            }
+        }
+    }
+
+    public isBlocked(msg: OmnisharpSessionMessage): boolean {
+        if (msg.type === 'buffer-template') {
+            // if the session is already initialized, it should belong to a previously queued context,
+            // or if there's already an inflight buffer-template
+            // otherwise, it should not block as we're waiting for this
+            return this.ready || this.templatedBlockedOnSession(); 
         } else if (msg.type === 'edit') {
-            return session.lockedByEdit || session.lockedByOperation || !session.ready;
+            // edits block on everything
+            // console.log('isBlocked edit (this.ready)', !this.ready);
+            return !this.ready || this.editBlockedOnOperation();
+        } else if (msg.type === 'codecheck' || msg.type === 'autocompletion') {
+            // these operations only block if there's an edit inflight/queued or if the session isnt ready
+            return !this.ready || this.operationBlockedOnEdit();
+        } else if (msg.type === 'context') {
+            // context blocks on anything in the queues.
+            return this.queue.length > 0 || this.awaiting.length > 0;
         }
+        return true;
     }
 
-    private aquireSession(type: string, session: SessionState) {
-        const currVal = session.ready;
-        if (type === 'edit') {
-            session.lockedByEdit = true;
-        }
-        if (type === 'codecheck' || type === 'autocompletion' || type === 'buffer-template') {
-            session.operationsInFlight++;
-            session.lockedByOperation = true;
-        }
-        if (type === 'context') {
-            session.ready = false;
-        }
-        // console.log('aquireSession', performance.now(), type, currVal, '=>', session.ready);
-    }
-
-    private releaseSession(type: string, session: SessionState) {
-        const currVal = session.ready;
-        if (type === 'edit') {
-            session.lockedByEdit = false;
-        } else if (type === 'codecheck' || type === 'autocompletion' || type === 'buffer-template') {
-            session.operationsInFlight--;
-            session.lockedByOperation = session.operationsInFlight > 0;
-        }
-        if (type === 'buffer-template') {
-            session.ready = true
-        }
-        if (session.ready && !session.lockedByEdit && session.queue.length > 0) {
-            const nextOp = session.queue.shift();
-            this.aquireSession(nextOp.msg.type, session);
-            nextOp.dequeue();
-            while (session.queue.length > 0 && !this.isBlocked(session.queue[0].msg)) {
-                const nextOpOp = session.queue.shift();
-                session.operationsInFlight++;
-                nextOpOp.dequeue();
-            }
-        } else if (!session.ready && !session.lockedByOperation) {
-            const bufferOp = session.queue.find(x => x.msg.type === 'buffer-template');
-            if (bufferOp) {
-                const idx = this.getBufferTemplateOffset(session);
-                const nextOp = session.queue.splice(idx, 1)[0];
-                this.aquireSession(nextOp.msg.type, session);
-                nextOp.dequeue();
-                // console.log('session not ready, but found buffer op');
-            }
-        }
-    }
-
-    private getBufferTemplateOffset(session: SessionState) {
+    private getBufferTemplateOffset() {
         let index = undefined;
-        // console.log('getBufferTemplateOffset', session.queue.map(x => x.msg.type).join(','))
-        session.queue.forEach((x, idx) => {
+        this.queue.forEach((x, idx) => {
             if (index === undefined && x.msg.type === 'buffer-template') {
                 index = idx;
             }
@@ -131,34 +120,50 @@ export class OmnisharpSynchronizer {
         return index;
     }
 
-    private releasedNextOperation(session: SessionState) {
-        if (session.ready && session.queue.length > 0) {
-            // console.log('releasedNextOperation READY')
-            const nextOp = session.queue.shift();
-            this.aquireSession(nextOp.msg.type, session);
-            nextOp.dequeue();
-            // dequeue all other non-edit operations on the buffer
-            while (session.queue.length > 0 && !this.isBlocked(session.queue[0].msg)) {
-                const nextOpOp = session.queue.shift();
-                session.operationsInFlight++;
-                nextOpOp.dequeue();
+    private templatedBlockedOnSession(): boolean {
+        return this.awaiting.filter(x => ['buffer-template', 'context'].indexOf(x.msg.type) > -1).length > 0;
+    }
+
+    private operationBlockedOnEdit(): boolean {
+        return this.awaiting.filter(x => ['edit'].indexOf(x.msg.type) > -1).length > 0 ||
+            this.queue.filter(x => ['edit', 'context'].indexOf(x.msg.type) > -1).length > 0;
+    }
+
+    private editBlockedOnOperation(): boolean {
+        return this.awaiting.filter(x => ['edit', 'codecheck', 'autocompletion', 'buffer-template'].indexOf(x.msg.type) > -1).length > 0;
+    }
+
+    private getInitalOperationCount() {
+        let index = undefined;
+        for(let i = 0; i < this.queue.length; i++) {
+            if (['codecheck', 'autocompletion'].indexOf(this.queue[i].msg.type) === -1) {
+                break;
+            } else {
+                index = i;
             }
-        } else {
-            // console.log('releasedNextOperation NOT READY')
         }
+        return index !== undefined ? index + 1 : index;
+    }
+}
+
+export class OmnisharpSynchronizer {
+    private session: SessionState[] = [];
+    
+    public resolveOperation(msg: OmnisharpSessionMessage): void {
+        const session = this.getSession(msg);
+        session.resolveOperation(msg);
+    }
+
+    public queueOperation(msg: OmnisharpSessionMessage): Promise<number> {
+        const session = this.getSession(msg);
+        return session.queueOperation(msg);
     }
 
     private getSession(msg: OmnisharpSessionMessage) {
         if (!this.session.find(x => x.id === msg.sessionId)) {
             Assert(msg.type === 'context', 'Type context msg when no session found');
-            this.session.push({
-                ready: false,
-                id: msg.sessionId,
-                lockedByEdit: false,
-                lockedByOperation: false,
-                operationsInFlight: 0,
-                queue: []
-            });
+            const newSession = new SessionState(msg.sessionId)
+            this.session.push(newSession);
         }
         return this.session.find(x => x.id === msg.sessionId);
     }

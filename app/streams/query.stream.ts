58579f701e2cc4dec6e2bf183121c7d6736f9db4
Svend Tofte <stofte@gmail.com>
2016-10-08 01:27:28 +0200

    Pass original timestamp for debug use

--- app/streams/query.stream.ts
+++ app/streams/query.stream.ts
@@ -102,7 +102,7 @@ export class QueryStream {
                                     lineOffset: data.LineOffset,
                                     defaultQuery: data.DefaultQuery,
                                     connectionId
-                                });
+                                }, sessionMsg.timestamp);
                             });
                     });
             })

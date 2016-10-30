69632ae0b331edafe39c850eca3a046a9e3fc94f
Svend Tofte <stofte@gmail.com>
2016-10-30 15:23:57 +0100

    Add blank page when no tab is active

--- app/components/query-panel.component.ts
+++ app/components/query-panel.component.ts
@@ -4,9 +4,14 @@ import { TabService } from '../services/tab.service';
 @Component({
     selector: 'rm-query-panel',
     template: `
-    <rm-controls></rm-controls>
-    <rm-editor></rm-editor>
-    <rm-result-display></rm-result-display>
+    <div class="rm-query-panel__session" *ngIf="this.sessionId">
+        <rm-controls></rm-controls>
+        <rm-editor></rm-editor>
+        <rm-result-display></rm-result-display>
+    </div>
+    <div class="rm-query-panel__blank" *ngIf="!this.sessionId">
+        no session
+    </div>
 `
 })
 export class QueryPanelComponent {

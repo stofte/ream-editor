6986dc74f2d598ce4e9aebba46314ae9eacf4ff8
Svend Tofte <stofte@gmail.com>
2016-11-02 01:10:58 +0100

    Configure and tweak handsontable

--- app/components/result-display.component.ts
+++ app/components/result-display.component.ts
@@ -15,27 +15,43 @@ class ColumnSizing {
     template: `
     <div class="rm-result-display__output-list">
         <div *ngFor="let output of outputList; let idx = index;">
-            <mdl-chip mdl-label="{{output.text}}" (click)="selectResult(idx)">
-                <span mdl-chip-contact class="mdl-color--blue mdl-color-text--white">
-                    <iron-icon icon="vaadin-icons:{{output.icon}}"></iron-icon>
-                </span>
-            </mdl-chip>
+            <paper-button (click)="selectResult(idx)" class="{{ idx === activePage ? 'active' : '' }}">
+                <iron-icon icon="vaadin-icons:{{output.icon}}"></iron-icon>
+                {{output.text}}
+            </paper-button>
         </div>
     </div>
     <div class="rm-result-display__table">
-
+        <div class="rm-result-display__table__hot"></div>
+        <div class="rm-result-display__table__hider" *ngIf="enableHider"></div>
     </div>
 `
 })
 export class ResultDisplayComponent implements AfterViewInit {
     resultPages: ResultPage[] = [];
     outputList = [];
+    activePage: number = null;
+
+    loadingPages: ResultPage[] = [];
+    loadingList = [];
+
+    enableHider = true;
 
     handsontableElm: any;
     tableOptions = {
         data: [],
+        columns: [],
         colHeaders: [],
-        afterLoadData: null
+        afterLoadData: null,
+        stretchH: 'last',
+        height: null,
+        rowHeaders: true,
+        manualColumnResize: true,
+        manualRowResize: true,
+        fillHandle: false,
+        // copy-limit is very confusing for enduser, maybe do something else ...
+        copyRowsLimit: 32000,
+        copyColsLimit: 32000
     };
 
     constructor(
@@ -46,48 +62,62 @@ export class ResultDisplayComponent implements AfterViewInit {
         output.events
             .filter(msg => msg.name === EventName.ResultStart)
             .subscribe(msg => {
-                this.outputList = [];
-                this.resultPages = [];
+                this.loadingPages = [];
+                this.loadingList = [];
             });
+
         output.events
             .filter(msg => msg.name === EventName.ResultUpdate)
             .subscribe(msg => {
                 const tbl = <ResultPage> msg.data;
                 const isFirst = this.resultPages.length === 0;
-                this.resultPages.push(tbl);
-                this.outputList.push({
+                this.loadingPages.push(tbl);
+                this.loadingList.push({
                     text: tbl.title,
-                    icon: 'table'
+                    icon: 'table',
+                    count: tbl.rows.length
                 });
-                if (isFirst) {
-                    this.selectResult(0);
-                }
             });
         output.events
             .filter(msg => msg.name === EventName.ResultDone)
             .subscribe(msg => {
-                console.log('results done', msg.id);
+                this.enableHider = false;
+                this.outputList = [...this.loadingList];
+                this.resultPages = [...this.loadingPages];
+                if (this.resultPages.length > 0) {
+                    this.selectResult(0);
+                }
             });
     }
 
     ngAfterViewInit() {
-        const container = this.elm.nativeElement.querySelector('.rm-result-display__table');
+        const container = this.elm.nativeElement.querySelector('.rm-result-display__table__hot');
         this.tableOptions.afterLoadData = (isFirstLoad) => {
             if (!isFirstLoad) {
                 this.handsontableElm.render();
             }
-        }
+        };
+        this.tableOptions.height = () => {
+            const h = this.elm.nativeElement.querySelector('.rm-result-display__table').clientHeight;
+            return ;
+        };
         this.handsontableElm = new Handsontable(container, this.tableOptions);
     }
 
     selectResult(idx: number) {
+        this.activePage = idx;
         const page = this.resultPages[idx];
         const data = [];
         page.rows.forEach(row => {
             data.push(page.isAtomic ? [row] : row);
         });
         this.handsontableElm.updateSettings({
-            colHeaders: [...page.columns]
+            colHeaders: [...page.columns],
+            columns: page.columns.map(col => {
+                return {
+                    editor: false
+                };
+            })
         });
         this.handsontableElm.loadData(data);
     }

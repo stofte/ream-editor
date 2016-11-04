52411c5e6c83bcf6c6f6cace1095946e3047f1e5
Svend Tofte <stofte@gmail.com>
2016-11-04 21:15:11 +0100

    Styled control row

--- app/components/controls.component.ts
+++ app/components/controls.component.ts
@@ -9,24 +9,21 @@ import { EditorDirective } from '../directives/editor.directive';
 import { Connection } from '../models/index';
 import { InputStream, OutputStream, EventName } from '../streams/index';
 
+const codeLabel = '<Code>';
 @Component({
     selector: 'rm-controls',
     template: `
-    <paper-button raised [disabled]="playDisabled" (click)="playClickHandler()"><mdl-icon>play_arrow</mdl-icon></paper-button>
-    <paper-dropdown-menu label="Context">
-        <paper-listbox class="dropdown-content"
-            [selected]="0"
-            (iron-select)="contextSelected($event)">
-            <paper-item *ngFor="let ctx of contextList">{{ctx}}</paper-item>
-        </paper-listbox>
-    </paper-dropdown-menu>
+    <button class="rm-controls__run"
+        [disabled]="playDisabled"><i class="material-icons">play_arrow</i></button>
+    <select #sel (change)="contextChanged($event, sel)">
+        <option *ngFor="let ctx of contextList" [value]="ctx.id">{{contextLabel(ctx.id)}}</option>
+    </select>
 `
 })
-export class ControlsComponent implements AfterViewInit {
-    contextList: string[] = ['Code'];
-    connections: Connection[] = [];
-    selector: any;
+export class ControlsComponent {
+    contextList: Connection[] = [<Connection> {id: null}];
     sessionId: string = null;
+    selectedContext = null;
     playDisabled = false;
     constructor(
         connections: ConnectionService,
@@ -36,38 +33,47 @@ export class ControlsComponent implements AfterViewInit {
         private elm: ElementRef
     ) {
         connections.all.subscribe(conns => {
-            this.connections = conns;
-            this.contextList = ['Code'].concat(conns.map(x => x.connectionString));
+            this.contextList = [<Connection> {connectionString: codeLabel}]
+                .concat([...conns]);
         });
         tabs.currentSessionId.filter(x => !!x).subscribe(id => {
-            this.sessionId = id;
-            if (this.selector) {
-                const context = tabs.sessionContext(id);
-                if (context !== null && context !== undefined) {
-                    for (let i = 0; i < this.connections.length; i++) {
-                        if (this.connections[i].id === context) {
-                            this.selector.selected = i + 1;
-                            break;
-                        }
-                    }
-                } else {
-                    // code context
-                    this.selector.selected = 0;
-                }
-            }
+            // this.sessionId = id;
+            // if (this.selector) {
+            //     const context = tabs.sessionContext(id);
+            //     if (context !== null && context !== undefined) {
+            //         for (let i = 0; i < this.connections.length; i++) {
+            //             if (this.connections[i].id === context) {
+            //                 this.selector.selected = i + 1;
+            //                 break;
+            //             }
+            //         }
+            //     } else {
+            //         // code context
+            //         this.selector.selected = 0;
+            //     }
+            // }
         });
     }
 
-    ngAfterViewInit() {
-        this.selector = this.elm.nativeElement.querySelector('paper-listbox');
-    }
-
-    contextSelected(event: any) {
-        const idx = this.selector.selected;
-        const ctx = idx === 0 ? null : this.connections[idx - 1];
-        console.log('contextSelected');
+    contextChanged(event: any, select: HTMLSelectElement) {
+        let ctx: any = null;
+        if (select.value) {
+            const id = parseInt(select.value, 10);
+            ctx = this.contextList.find(x => x.id === id);
+        }
         this.tabs.setContext(this.sessionId, ctx);
     }
+
+    contextLabel(id: number) {
+        return id ? this.contextList.find(x => x.id === id).connectionString : 'Code';
+    }
+
+    // contextSelected(event: any) {
+    //     const idx = this.selector.selected;
+    //     const ctx = idx === 0 ? null : this.connections[idx - 1];
+    //     console.log('contextSelected');
+    //     this.tabs.setContext(this.sessionId, ctx);
+    // }
 
     playClickHandler() {
         const id = this.sessionId;
